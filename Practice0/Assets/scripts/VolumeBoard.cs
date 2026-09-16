048c8bf0186ae5892819f88291493eb0eb42ea0b
lemoncube12@gmail.com <lemoncube12@gmail.com>
2026-09-16 11:32:52 +0900

    [Feat] UI 구성 스크립트 실습

---
+++ Practice0/Assets/scripts/VolumeBoard.cs
@@ -0,0 +1,35 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeBoard : MonoBehaviour
+{
+    [SerializeField] private Slider _volumeSlider;
+    [SerializeField] private TextMeshProUGUI _volumeText;
+
+    
+    private void Start()
+    {
+        BindSliderEvents();
+        UpdateText(_volumeSlider.value);
+
+    }
+
+    private void BindSliderEvents()
+    {
+        _volumeSlider.onValueChanged.AddListener(OnVolumeChanged);
+    }
+
+    private void OnVolumeChanged(float volume)
+    {
+        UpdateText(volume);
+      
+    }
+
+    private void UpdateText(float volume)
+    {
+        _volumeText.text = "Volume: " + volume.ToString();
+    }
+
+
+}
