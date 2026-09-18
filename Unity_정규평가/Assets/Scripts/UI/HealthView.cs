eb55949897e903ec927a88ce3f23764e7ecb40ca
lemoncube12@gmail.com <lemoncube12@gmail.com>
2026-09-18 17:03:51 +0900

    [Feat] 3번 체력값 수정

--- Unity_정규평가/Assets/Scripts/UI/HealthView.cs
+++ Unity_정규평가/Assets/Scripts/UI/HealthView.cs
@@ -3,7 +3,7 @@ using TMPro;
 
 public class HealthView : MonoBehaviour
 {
-    private TextMeshProUGUI _healthText;
+    public TextMeshProUGUI _healthText;
 
     public void Show(int health)
     {
